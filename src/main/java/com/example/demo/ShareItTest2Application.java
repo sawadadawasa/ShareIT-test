79c7c60d555c55a633e7c2ae7633958b8ca90bcc
kentaro.mimata <kentaro.mimata@rakus.co.jp>
2019-08-05 15:06:27 +0900

    first commit

---
+++ src/main/java/com/example/demo/ShareItTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareItTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareItTest2Application.class, args);
+	}
+
+}
