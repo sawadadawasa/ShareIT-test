2a7539acb12f1330c08f533680dcd11d7146755e
kentaro.mimata <kentaro.mimata@rakus.co.jp>
2019-08-05 15:43:42 +0900

    エラーが吐くように記述

--- src/main/java/com/example/demo/ShareItTest2Application.java
+++ src/main/java/com/example/demo/ShareItTest2Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShareItTest2Application {
 
-	public static void main(String[] args) {
+	sawada public static void main(String[] args) {
 		SpringApplication.run(ShareItTest2Application.class, args);
 	}
 
